23cb38537854b7985b385e309939cbd2666ad0bd
Andredossantos96 <86696902+Andredossantos96@users.noreply.github.com>
2022-08-29 22:27:03 -0300

    primeiro commit

---
+++ hello-world/src/Rodape.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Rodape(){
+    return (
+        <footer>
+            <p>isso é o Rodape da pagina</p>
+        </footer>
+    )
+    }
+    
+export default Rodape;
---
+++ hello-world/src/Topo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Topo(){
+    return (
+        <div>
+            <h1>isso é o topo da pagina</h1>
+        </div>
+    )
+    }
+    
+export default Topo;
